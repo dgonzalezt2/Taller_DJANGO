5bded7b6b926167abe4d616f0f631484ee0a7787
dgonzalezt <dgonzalez2@eafit.edu.co>
2023-02-09 11:06:33 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -6,4 +6,4 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>WELCOME TO HOME PAGE</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html',{'name':'Greg Lim'})
+    return render(request, 'home.html',{'name':'David Gonzalez'})
